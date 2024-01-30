a4c3a24441bd2570ffb67c162aeefc903bd4d00b
Jose Emmanuel Quispe <jequispe@inteia.com.co>
2024-01-30 18:17:51 -0500

    fix port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.static('public'));
 
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(process.env.PORT || PORT, () => console.log(`Server listening on port: ${PORT}`));
